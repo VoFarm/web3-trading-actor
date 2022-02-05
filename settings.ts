04fe3d27bcb93af47f92320b8aadce54de8721d5
Nikita Trotner <nikita@ttnr.me>
2022-02-05 01:21:05 +0100

    added settings

---
+++ settings.ts
@@ -0,0 +1,15 @@
+import {ITradingActor} from "./types/trading-actor.ts";
+
+export const user: ITradingActor = {
+    publicKey: "",
+    privateKey: ""
+}
+export const actorNet = {
+    url: "wss://rinkeby.infura.io/ws/v3/",
+    netID: 4
+}
+export const pairPricerNet = {
+    url: "wss://mainnet.infura.io/ws/v3/"
+}
+
+export const loopSleepSeconds: number = 60
