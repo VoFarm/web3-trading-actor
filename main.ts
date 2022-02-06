6c789d6c9002cdb2089e9c4ea6ca7545e2a566ce
Nikita Trotner <nikita@ttnr.me>
2022-02-06 13:09:43 +0100

    seperated bot from ui

--- main.ts
+++ main.ts
@@ -10,81 +10,73 @@ import {opineCors} from "https://deno.land/x/cors/mod.ts";
 
 let performanceTime: number[] = []
 
+const storage = new Storage()
+const actor = new TradingActor(actorNet.url, user, TradingContractAddress, TradingContractABI, actorNet.netID, storage)
+const pairPricer = new PairPricer(pairPricerNet.url, UniswapContractABI)
+
+function startServer() {
+    const app = opine();
+    app.use(opineCors())
+
+    app.get("/listening", (req, res) => {
+        res.send(String(actor.listeningEvent));
+    });
+
+    app.get("/log", (req, res) => {
+        res.json(storage.getConsoleLog());
+    });
+
+    app.get("/iteration", (req, res) => {
+        res.json(storage.getIteration(req.query.id));
+    });
+
+    app.get("/count", (req, res) => {
+        res.send(String(storage.getCounter() - 1));
+    });
+
+    app.get("/", (req, res) => {
+        res.sendFile(Deno.cwd() + "/main.html");
+    });
+
+    app.listen(3001, () => console.log("Starting at: http://localhost:3001"));
+}
+
 async function startBot() {
-    return new Promise(((resolve, reject) => {
-        const app = opine();
-        app.use(opineCors())
+    return new Promise((async (resolve, reject) => {
+        for (let amountLoops = 0; amountLoops < 60; amountLoops++) {
+            try {
+                // wait for event
+                const eventPromise = actor.getEventOutput("requestData")
+                let _ = actor.callContractSwap();
+                const response = await eventPromise
 
-        const storage = new Storage()
-        const actor = new TradingActor(actorNet.url, user, TradingContractAddress, TradingContractABI, actorNet.netID, storage)
-        const pairPricer = new PairPricer(pairPricerNet.url, UniswapContractABI)
+                var startTime = performance.now()
 
-        app.get("/listening", (req, res) => {
-            res.send(String(actor.listeningEvent));
-        });
-
-        app.get("/log", (req, res) => {
-            res.json(storage.getConsoleLog());
-        });
-
-        app.get("/iteration", (req, res) => {
-            res.json(storage.getIteration(req.query.id));
-        });
-
-        app.get("/count", (req, res) => {
-            res.send(String(storage.getCounter() - 1));
-        });
-
-        app.get("/", (req, res) => {
-            res.sendFile(Deno.cwd() + "/main.html");
-        });
-
-        app.listen(3001, () => console.log("Starting at: http://localhost:3001"));
-
-        const tradingLoop = new Promise((async (resolve, reject) => {
-            for (let amountLoops = 0; amountLoops < 60; amountLoops++) {
-                try {
-                    // wait for event
-                    const eventPromise = actor.getEventOutput("requestData")
-                    let _ = actor.callContractSwap();
-                    const response = await eventPromise
-
-                    var startTime = performance.now()
-
-                    // call uniswap contract
-                    let uniswapResponse: UniswapPoolResponse = await pairPricer.selectTokenPair(response.tknPair)
-                    if (uniswapResponse.price === "NaN") {
-                        storage.addConsoleLog(`ERROR: Switch Case Failed for Uniswap with Pair: ${response.tknPair}`)
-                    } else {
-                        await actor.callback(response.id, uniswapResponse.price)
-                    }
-
-                    var endTime = performance.now()
-
-                    if (performanceTime.length >= 25)
-                        performanceTime.shift()
-                    performanceTime.push(endTime - startTime)
-                    let date = new Date()
-                    storage.addConsoleLog(`Finished Contract Swap Call | Time: ${date.toDateString()} ${date.toTimeString()} | Pair: ${response.tknPair} | Performance: ${endTime - startTime} ms`)
-                    await new Promise(resolve => setTimeout(resolve, loopSleepSeconds * 1000));
-                } catch (e) {
-                    storage.addConsoleLog(e)
-                    reject(true)
+                // call uniswap contract
+                let uniswapResponse: UniswapPoolResponse = await pairPricer.selectTokenPair(response.tknPair)
+                if (uniswapResponse.price === "NaN") {
+                    storage.addConsoleLog(`ERROR: Switch Case Failed for Uniswap with Pair: ${response.tknPair}`)
+                } else {
+                    await actor.callback(response.id, uniswapResponse.price)
                 }
+
+                var endTime = performance.now()
+
+                if (performanceTime.length >= 25)
+                    performanceTime.shift()
+                performanceTime.push(endTime - startTime)
+                let date = new Date()
+                storage.addConsoleLog(`Finished Contract Swap Call | Time: ${date.toDateString()} ${date.toTimeString()} | Pair: ${response.tknPair} | Performance: ${endTime - startTime} ms`)
+                await new Promise(resolve => setTimeout(resolve, loopSleepSeconds * 1000));
+            } catch (e) {
+                storage.addConsoleLog(e)
+                resolve(e)
             }
-            resolve(true)
-        }));
-    }))
-}
-
-async function initializeLoop() {
-    while (1) {
-        try {
-            await startBot()
-        } catch {
-            await new Promise(resolve => setTimeout(resolve, loopSleepSeconds * 1000));
         }
-    }
+        resolve(true)
+    }));
 }
+startServer()
 
-await initializeLoop()
+startBot().then(() => startBot()).catch(() => startBot())
+
