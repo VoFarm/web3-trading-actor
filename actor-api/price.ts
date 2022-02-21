b776be2a0a7cf6cf42b676db6f3e1b960a627529
Nikita Trotner <nikita@ttnr.me>
2022-02-21 15:40:09 +0100

    documentation & refactoring (#11)
    * restructured endpoints for main and added documentation
    
    * restructured bot
    
    * fixed range
    
    * include all project files for formatter and linter

---
+++ actor-api/price.ts
@@ -0,0 +1,54 @@
+import { Storage } from '../contract-actor/components/storage.ts';
+import { Price } from '../types/storage.ts';
+import { Opine } from 'https://raw.githubusercontent.com/ntrotner/opine/main/src/types.ts';
+
+/**
+ * add all endpoints to the app related to the price and token amounts
+ */
+export function initializePriceAPI(app: Opine): Opine {
+  /**
+   * returns the amount of tokens at position ?id=number
+   *
+   * @returns Price
+   */
+  app.get('/price', async (req, res) => {
+    try {
+      res.json(await Storage.getPrice(req.query.id));
+    } catch {
+      // return null if number wasn't valid or out of range
+      res.json(null);
+    }
+  });
+
+  /**
+   * returns all prices starting at position ?lastID=number
+   *
+   * @returns Price[]
+   */
+  app.get('/priceRange', async (req, res) => {
+    try {
+      const allPrices: (Price | null)[] = [];
+      const counter = await Storage.getPriceCounter();
+      const lastID = Number(req.query.lastID);
+
+      // return empty if id isn't in range
+      if (counter < lastID) {
+        res.json([]);
+        return;
+      }
+
+      // get past prices
+      for (let i = counter; i >= (lastID < 0 ? 0 : lastID); i--) {
+        allPrices.push(await Storage.getPrice(i));
+      }
+
+      // return list of prices
+      res.json(allPrices);
+    } catch {
+      // return empty list if number couldn't be parsed or storage error
+      res.json([]);
+    }
+  });
+
+  return app;
+}
